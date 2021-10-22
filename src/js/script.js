1ea1171c426cca9ec14debc009f14794ee2a47ea
Hemsundar Paranthaman <hemsundar.paranthaman@outlook.com>
2021-10-22 18:14:56 +0530

    Completed Home

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+import '../scss/style.scss';
+
+const navHamburger = document.querySelector('.nav__hamburger');
+
+navHamburger.addEventListener("click", () => {
+    const navLinks = document.querySelector(".nav__links");
+    const html = document.querySelector("html");
+
+    navHamburger.classList.toggle("nav__hamburger--active");
+    navLinks.classList.toggle("nav__links--active");
+    html.classList.toggle("html--active");
+});
